58e7c0149ae0d04fbbdec11c23685921712c9516
Jayant Bhawal <bhawal.jayant@gmail.com>
2018-08-28 12:13:16 +0530

    Turn arrow in InPlaceTooltip into a separate element, and add support for absolute positioning of that arrow using a prop.

--- src/components/InPlaceTooltip/index.js
+++ src/components/InPlaceTooltip/index.js
@@ -4,16 +4,28 @@ import cls from 'classnames';
 import css from './styles.module.css';
 
 type Props = {
-	position?: string,
-	className?: string
+	position?: string,  // left | right | center | absolute
+	className?: string,
+	absoluteValue?: number
 }
 
 export class InPlaceTooltip extends Component<Props> {
 	render() {
-		let { position = 'center', className = '' } = this.props;
+		let { position = 'absolute', className = '', absoluteValue = 33 } = this.props;
+
+		let absolutePositioningStyles = {};
+		if (position === 'absolute') {
+			let min = 2, max = 94; // browser tested values for the left property on the tooltip arrow
+			let left = ((max - min) * (absoluteValue / 100)) + min;
+			absolutePositioningStyles = {
+				right: 'unset',
+				left: `${left}%`
+			}
+		}
 		return (
 			<div className={cls(css.tooltip, css[`tooltip--${position}`], 'component--in-place-tooltip', `component--in-place-tooltip--${position}`, className)}>
 				{this.props.children}
+				<div className={cls(css.arrow, 'component--in-place-tooltip--arrow')} style={absolutePositioningStyles} />
 			</div>
 		);
 	}
