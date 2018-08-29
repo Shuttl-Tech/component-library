0405a4f06185f83301ad9ccf5354cb7cfd72e18c
Jayant Bhawal <bhawal.jayant@gmail.com>
2018-08-29 12:59:30 +0530

    Build version 0.1.2.

---
+++ src/build-commit.js
@@ -0,0 +1,13 @@
+// Utility to make a build commit with a version number
+
+const fs = require('fs');
+const { spawnSync } = require('child_process');
+
+const { version } = JSON.parse(fs.readFileSync('./package.json', { encoding: 'utf-8' }));
+
+const options = {
+	stdio: 'inherit'
+};
+
+spawnSync('git', ['add', '.'], options);
+spawnSync('git', ['commit', '-m', `Build version ${version}.`], options);
---
+++ src/version-bump.js
@@ -0,0 +1,30 @@
+// Utility to upgrade package.json version
+
+const fs = require('fs');
+const { titleCase } = require('voca');
+
+const UPGRADE_MODE = { PATCH: 'patch',
+	MINOR: 'minor',
+	MAJOR: 'major'
+};
+
+const UPGRADE_MODES = [UPGRADE_MODE.PATCH, UPGRADE_MODE.MINOR, UPGRADE_MODE.MAJOR];
+
+const UPGRADE = process.env.UPGRADE && (UPGRADE_MODES.includes(process.env.UPGRADE.toLowerCase()))? process.env.UPGRADE : UPGRADE_MODE.PATCH;
+
+let file = JSON.parse(fs.readFileSync('./package.json', { encoding: 'utf-8' }));
+let oldVersion = file.version;
+let [major, minor, patch] = oldVersion.split('.').map(n => parseInt(n, 10));
+
+switch (UPGRADE) {
+	case UPGRADE_MODE.MINOR: minor++; break;
+	case UPGRADE_MODE.MAJOR: major++; break;
+	default: /* PATCH */ patch++;
+}
+
+let version = [major, minor, patch].join('.');
+file = JSON.stringify({ ...file, version }, null, '\t') + '\n';
+
+fs.writeFileSync('./package.json', file);
+
+console.log(`${titleCase(UPGRADE)} upgrade from version ${oldVersion} to ${version} 🎉`);
