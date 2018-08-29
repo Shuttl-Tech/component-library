eddfdbbd288fb72475a9663f22af27b87b503f10
Jayant Bhawal <bhawal.jayant@gmail.com>
2018-08-29 11:01:29 +0530

    Add global classnames to CollapsibleBox.

--- src/components/CollapsibleBox/index.js
+++ src/components/CollapsibleBox/index.js
@@ -41,19 +41,23 @@ export class CollapsibleBox extends Component<Props> {
 		footer = <div className={cls('component--collapsible-box--footer')} onClick={() => this.toggleExpansion()}>{footer}</div>;
 
 		let expansionStateClass;
+		// if component state says it's expanded
 		if (isExpanded) {
-			if (expanded === 'fully') expansionStateClass = css.expanded;
-			else if (expanded === 'partially') expansionStateClass = css['expanded-partially'];
+			// if component prop says it can be fully expanded
+			if (expanded === 'fully') expansionStateClass = cls(css.expanded, 'component--collapsible-box--container--expanded'); // then add the expanded class
+			else if (expanded === 'partially') expansionStateClass = cls(css['expanded-partially'], 'component--collapsible-box--container--expanded-partially'); // then add the expanded partially class.
 		}
-		else if (collapsed === 'fully') expansionStateClass = css[ 'collapsed-fully' ];
+		else if (collapsed === 'fully') expansionStateClass = cls(css[ 'collapsed-fully' ], 'component--collapsible-box--container--collapsed-fully');
+
+		let fullyCollapsibleClass = collapsed === 'fully' ? cls(css['fully-collapsible'], 'component--collapsible-box--container--fully-collapsible') : '';
 
 		return (
 			<ModularBox
-				className={cls(css.container, collapsed === 'fully' ? css['fully-collapsible'] : '', expansionStateClass, className)}
+				className={cls(css.container, fullyCollapsibleClass, expansionStateClass, className)}
 				header={header}
 				footer={footer}
-				headerClassName={headerClassName}
-				bodyClassName={cls(css.body, expansionStateClass, bodyClassName)}
+				headerClassName={cls(headerClassName, 'component--collapsible-box--header')}
+				bodyClassName={cls(css.body, expansionStateClass, bodyClassName, 'component--collapsible-box--body')}
 				footerClassName={cls(collapsed === 'fully' && !isExpanded ? css['footer-no-border'] : '', footerClassName)}
 			>
 				{body}
