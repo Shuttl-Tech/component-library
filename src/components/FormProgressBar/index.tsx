503dad1e3078e7ee0b65a51f7cf0900200d474e3
Shuttl <nancy.32721@gmail.com>
2019-09-23 19:47:55 +0530

    added form progress bar component

---
+++ src/components/FormProgressBar/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import cls from 'classnames';
+
+import css from './styles.module.scss';
+
+type Props = {
+  formSteps: Array<number>;
+  formStep: number;
+  onStepClick: Function;
+  isComplete?: boolean;
+}
+
+const FormProgressBar = ({ formSteps, formStep, onStepClick, isComplete }: Props ) => {
+  const lastFormStep = formSteps.length - 1;
+  const progress = Math.floor(((lastFormStep - formStep) / lastFormStep) * 100);
+
+  return (
+    <div className={cls(css.progress, isComplete && css['progress--complete'])}>
+      <div
+        className={css['progress-bar']}
+        style={{
+          transform: `translateX(-${progress}%)`,
+        }}
+      />
+      {formSteps.map(stepIndex => {
+        return (
+          <div className={css['step-bubble-wrapper']} key={stepIndex} onClick={() => onStepClick(stepIndex)}>
+            <div className={cls(css['step-bubble'], formStep === stepIndex && css.active)}>
+              <span>{stepIndex + 1}</span>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default FormProgressBar;
---
+++ stories/1-FormProgressBar.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+
+import FormProgressBar from 'components/FormProgressBar';
+
+export default {
+  title: 'FormProgressBar',
+};
+
+export const withCompleteSteps = () => (
+  <FormProgressBar formSteps={[0, 1, 2]} formStep={2}
+                   onStepClick={action('clicked')}/>
+);
+
+export const withOnlyOneStep = () => (
+  <FormProgressBar formSteps={[0, 1, 2]} formStep={1}
+                   onStepClick={action('clicked')}/>
+);
