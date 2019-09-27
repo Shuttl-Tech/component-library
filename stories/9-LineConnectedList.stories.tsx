c45656ed38a5809e4e4e0c57a66e46cf72b12322
anuporwal227 <anu.porwal@shuttl.com>
2019-09-27 18:22:52 +0530

    Added vertical list

---
+++ stories/9-LineConnectedList.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { LineConnectedList } from 'components/LineConnectedList';
+
+export default {
+  title: 'LineConnectedList',
+};
+
+export const text = () => <LineConnectedList/>;
