e715848bedcc8b3a3bb451e9ae1baf228593380d
anuporwal227 <anu.porwal@shuttl.com>
2019-10-09 16:47:26 +0530

    Formatted with prettier

--- src/components/HorizontalConnectedList/index.tsx
+++ src/components/HorizontalConnectedList/index.tsx
@@ -13,9 +13,9 @@ const Item = ({
   item,
   showLeftConnector,
   showRightConnector,
-  leftConnectorBorder= 'solid',
-  rightConnectorBorder='solid',
-  classes={}
+  leftConnectorBorder = 'solid',
+  rightConnectorBorder = 'solid',
+  classes = {},
 }: {
   item: Item;
   showLeftConnector: boolean;
@@ -23,19 +23,33 @@ const Item = ({
   leftConnectorBorder: string;
   rightConnectorBorder: string;
   classes?: {
-    list?:string,
-    listItem?: string,
-    icon?: string,
-    connector?: string,
-    content?: string
-  }
+    list?: string;
+    listItem?: string;
+    icon?: string;
+    connector?: string;
+    content?: string;
+  };
 }) => {
   return (
     <div className={cls(css.item, classes.listItem)}>
       <div className={cls(css.icon, classes.icon)}>
-        <div className={cls(css.connector, classes.connector, !showLeftConnector && css.hide, css[`border-${leftConnectorBorder}`])} />
+        <div
+          className={cls(
+            css.connector,
+            classes.connector,
+            !showLeftConnector && css.hide,
+            css[`border-${leftConnectorBorder}`]
+          )}
+        />
         <div>{item.icon}</div>
-        <div className={cls(css.connector, classes.connector, !showRightConnector && css.hide, css[`border-${rightConnectorBorder}`])} />
+        <div
+          className={cls(
+            css.connector,
+            classes.connector,
+            !showRightConnector && css.hide,
+            css[`border-${rightConnectorBorder}`]
+          )}
+        />
       </div>
       <div className={cls(css.content, classes.content)}>{item.content}</div>
     </div>
@@ -45,9 +59,9 @@ const Item = ({
 export type Props = HTMLAttributes<HTMLDivElement> & {
   items: Array<Item>;
   classes?: {
-    list?:string,
-    listItem?: string,
-  }
+    list?: string;
+    listItem?: string;
+  };
 };
 
 export const HorizontalConnectedList = ({ items, classes = {}, ...props }: Props) => {
@@ -61,7 +75,7 @@ export const HorizontalConnectedList = ({ items, classes = {}, ...props }: Props
             classes={classes}
             showLeftConnector={index !== 0}
             showRightConnector={index !== items.length - 1}
-            leftConnectorBorder={index > 0 ? items[index-1].borderStyle : item.borderStyle}
+            leftConnectorBorder={index > 0 ? items[index - 1].borderStyle : item.borderStyle}
             rightConnectorBorder={item.borderStyle}
           />
         );
